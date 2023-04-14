8aca52ced87eabccbbe8280bccc83117d2087764
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-13 23:47:48 -0300

    Just practicing a bit.

--- Topics/Working with strings/Name Card/src/Main.kt
+++ Topics/Working with strings/Name Card/src/Main.kt
@@ -1,7 +1,6 @@
 fun main() {
     val firstName = readln()
     val secondName = readln()
-    print(firstName[0])
-    print(". ")
-    print("$secondName")
+    print(firstName[0] + ". " + secondName)
+
 }
