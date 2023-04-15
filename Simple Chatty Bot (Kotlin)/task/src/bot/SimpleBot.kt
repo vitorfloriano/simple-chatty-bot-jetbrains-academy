f47a29c111d83c578740640acf524ee93d74f197
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-14 22:29:46 -0300

    Just practicing a bit.

--- Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -16,7 +16,15 @@ fun main() {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
-    // reading all remainders
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-    println("Your age is {yourAge}; that's a good time to start programming!")
+    println("Your age is " + age + "; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+
+    // read a number and count to it here
+
+    println("Completed, have a nice day!")
 }
