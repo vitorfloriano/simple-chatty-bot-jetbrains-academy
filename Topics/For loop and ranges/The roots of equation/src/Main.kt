02059a5ba5b841b277f4f0b65a2a18ac862fb86b
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-17 21:29:12 -0300

    Just practicing a bit.

--- Topics/For loop and ranges/The roots of equation/src/Main.kt
+++ Topics/For loop and ranges/The roots of equation/src/Main.kt
@@ -1,13 +1,13 @@
 fun main() {
-        val a = readLine()!!.toInt()
-        val b = readLine()!!.toInt()
-        val c = readLine()!!.toInt()
-        val d = readLine()!!.toInt()
-
-        for (x in 0..1000) {
-            val result = a * x * x * x + b * x * x + c * x + d
-            if (result == 0) {
-                println(x)
-            }
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    val c = readLine()!!.toInt()
+    val d = readLine()!!.toInt()
+    
+    for (x in 0..1000) {
+        val result = a * x * x * x + b * x * x + c * x + d
+        if (result == 0) {
+        println(x)
         }
+    }
 }
---
+++ Topics/Standard output/We need to learn Kotlin/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print(
+        """WE NEED
+
+TO LEARN KOTLIN
+
+AS QUICKLY AS POSSIBLE""".trimIndent())
+}
